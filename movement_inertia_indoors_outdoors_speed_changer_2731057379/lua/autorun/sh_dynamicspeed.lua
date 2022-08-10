c8127d0728130c0b955ca8562cbb5d520d6e45bf
relaxtakenotes <91839650+relaxtakenotes@users.noreply.github.com>
2022-08-10 23:05:32 +0500

    fixes + new features

--- movement_inertia_indoors_outdoors_speed_changer_2731057379/lua/autorun/sh_dynamicspeed.lua
+++ movement_inertia_indoors_outdoors_speed_changer_2731057379/lua/autorun/sh_dynamicspeed.lua
@@ -13,23 +13,27 @@ CreateConVar("sv_dynamicspeed_stepsoundtime_multiplier", 40000, {FCVAR_REPLICATE
 CreateConVar("sv_dynamicspeed_lerp_multiplier", 10, {FCVAR_REPLICATED, FCVAR_ARCHIVE}, "How fast you want to transition between speeds.")
 CreateConVar("sv_dynamicspeed_jump_distance", 4, {FCVAR_REPLICATED, FCVAR_ARCHIVE}, "Some magic number that changes the distance you jump.")
 CreateConVar("sv_dynamicspeed_enabled", 1, {FCVAR_REPLICATED, FCVAR_ARCHIVE}, "Quite obvious, isn't it?")
+CreateConVar("sv_dynamicspeed_crouch_speed", 0.3, {FCVAR_REPLICATED, FCVAR_ARCHIVE}, "Speed for Crouching.")
+CreateConVar("sv_dynamicspeed_footstep_workaround", 1, {FCVAR_REPLICATED, FCVAR_ARCHIVE}, "Enable a workaround for footsteps, which makes them play even if your speed is too low. (under 90 or under 60 when crouched)")
 
 local speeds = {}
 speeds["indoors"] = {}
 speeds["outdoors"] = {}
 
-speeds["indoors"]["slowwalk"] = GetConVar("sv_dynamicspeed_indoor_slowwalk"):GetInt()
-speeds["indoors"]["walk"] = GetConVar("sv_dynamicspeed_indoor_walk"):GetInt()
-speeds["indoors"]["run"] = GetConVar("sv_dynamicspeed_indoor_run"):GetInt()
-speeds["outdoors"]["slowwalk"] = GetConVar("sv_dynamicspeed_outdoor_slowwalk"):GetInt()
-speeds["outdoors"]["walk"] = GetConVar("sv_dynamicspeed_outdoor_walk"):GetInt()
-speeds["outdoors"]["run"] = GetConVar("sv_dynamicspeed_outdoor_run"):GetInt()
-
 local function traceableToSky(pos, offset)
     local tr = util.TraceLine({start=pos + offset, endpos=pos + Vector(offset.x, offset.y, 100000000), mask=MASK_NPCWORLDSTATIC})
 	local temp = util.TraceLine({start=tr.StartPos, endpos=pos, mask=MASK_NPCWORLDSTATIC}) -- doing this because sometimes the trace can go oob and even rarely there are cases where i cant see if it spawned oob
-    if temp.HitPos == pos and tr.HitSky then return true end
+    if temp.HitPos == pos and not temp.StartSolid and tr.HitSky then return true end
     return false
+end
+
+local function updateSpeed()
+	speeds["indoors"]["slowwalk"] = GetConVar("sv_dynamicspeed_indoor_slowwalk"):GetInt()
+	speeds["indoors"]["walk"] = GetConVar("sv_dynamicspeed_indoor_walk"):GetInt()
+	speeds["indoors"]["run"] = GetConVar("sv_dynamicspeed_indoor_run"):GetInt()
+	speeds["outdoors"]["slowwalk"] = GetConVar("sv_dynamicspeed_outdoor_slowwalk"):GetInt()
+	speeds["outdoors"]["walk"] = GetConVar("sv_dynamicspeed_outdoor_walk"):GetInt()
+	speeds["outdoors"]["run"] = GetConVar("sv_dynamicspeed_outdoor_run"):GetInt()
 end
 
 local function getOutdoorsState(pos)
@@ -44,35 +48,43 @@ end
 if clientInMultiplayer then
 	hook.Add("InitPostEntity", "dynamicspeed_sanity_client", function()
 		ply = LocalPlayer()
+		updateSpeed()
 		ply.ds_lerpFraction = 1
 		ply.ds_lerpValue = 1
-		ply.ds_lerpFrom = 0
-		ply.ds_lerpTo = 0
-		ply.ds_posState = "indoors"
-		ply.ds_posStatePrevious = "indoors"
+		ply.ds_posState = getOutdoorsState(ply:GetPos())
+		ply.ds_posStatePrevious = ply.ds_posState
 		ply.ds_walkState = "walk"
 		ply.ds_walkStatePrevious = "walk"
 		ply.ds_wasOnGround = true
+		ply.ds_lerpTo = speeds[ply.ds_posState][ply.ds_walkState]
+		ply.ds_lerpFrom = speeds[ply.ds_posStatePrevious][ply.ds_walkStatePrevious]
+		ply.ds_StepTimer = 0
 	end)
 end
 
 if not clientInMultiplayer then
 	hook.Add("PlayerSpawn", "dynamicspeed_sanity", function(ply,transition)
+		updateSpeed()
 		ply.ds_lerpFraction = 1
 		ply.ds_lerpValue = 1
-		ply.ds_lerpFrom = 0
-		ply.ds_lerpTo = 0
-		ply.ds_posState = "indoors"
-		ply.ds_posStatePrevious = "indoors"
+		ply.ds_posState = getOutdoorsState(ply:GetPos())
+		ply.ds_posStatePrevious = ply.ds_posState
 		ply.ds_walkState = "walk"
 		ply.ds_walkStatePrevious = "walk"
 		ply.ds_wasOnGround = true
+		ply.ds_lerpTo = speeds[ply.ds_posState][ply.ds_walkState]
+		ply.ds_lerpFrom = speeds[ply.ds_posStatePrevious][ply.ds_walkStatePrevious]
+		ply.ds_StepTimer = 0
 	end)
 end
 
 hook.Add("SetupMove", "dynamicspeed_think", function(ply,mv)
 	if clientInMultiplayer then if ply != LocalPlayer() then return end end
 	if GetConVar("sv_dynamicspeed_enabled"):GetInt() != 1 then return end
+
+	local moving = (ply:KeyDown(IN_FORWARD) or ply:KeyDown(IN_BACK) or ply:KeyDown(IN_MOVELEFT) or ply:KeyDown(IN_MOVERIGHT))
+
+	updateSpeed()
 
 	ply.ds_posStatePrevious = ply.ds_posState
 	ply.ds_posState = getOutdoorsState(ply:GetPos())
@@ -83,23 +95,18 @@ hook.Add("SetupMove", "dynamicspeed_think", function(ply,mv)
 	if ply:KeyDown(IN_WALK) then ply.ds_walkState = "slowwalk" end
 
 	if ply.ds_posState != ply.ds_posStatePrevious or ply.ds_walkState != ply.ds_walkStatePrevious then
-		if ply.ds_lerpValue == 1 then ply.ds_lerpValue = 0 else ply.ds_lerpValue = 1 - ply.ds_lerpValue end
+		ply.ds_lerpValue = 0
 		ply.ds_lerpTo = speeds[ply.ds_posState][ply.ds_walkState]
-		ply.ds_lerpFrom = speeds[ply.ds_posStatePrevious][ply.ds_walkStatePrevious]
+		ply.ds_lerpFrom = ply.ds_actualMaxSpeed
 		ply:SetRunSpeed(speeds[ply.ds_posState]["run"])
 		ply:SetWalkSpeed(speeds[ply.ds_posState]["walk"])
 		ply:SetSlowWalkSpeed(speeds[ply.ds_posState]["slowwalk"])
 	end
 
-	local maxspeed = Lerp(ply.ds_lerpValue, ply.ds_lerpFrom, ply.ds_lerpTo)
 	ply.ds_lerpValue = math.Clamp(ply.ds_lerpValue + ply.ds_lerpFraction * FrameTime() * GetConVar("sv_dynamicspeed_lerp_multiplier"):GetFloat(), 0, 1)
+	local maxspeed = Lerp(ply.ds_lerpValue, ply.ds_lerpFrom, ply.ds_lerpTo)
 
-	if not ply:KeyDown(IN_FORWARD) and not ply:KeyDown(IN_BACK) and not ply:KeyDown(IN_MOVELEFT) and not ply:KeyDown(IN_MOVERIGHT) then
-		if ply.ds_lerpTo < ply.ds_lerpFrom then ply.ds_lerpValue = 1 end
-		if ply.ds_lerpTo > ply.ds_lerpFrom then ply.ds_lerpValue = 0 end
-	end
-
-	if ply:KeyDown(IN_SPEED) and ply.ds_wasOnGround != ply:OnGround() and !ply:OnGround() and ply:WaterLevel() < 3 then
+	if ply:KeyDown(IN_SPEED) and ply.ds_wasOnGround != ply:OnGround() and ply:KeyDown(IN_JUMP) and !ply:OnGround() and ply:WaterLevel() < 3 then
 		ply:SetVelocity(Vector(-ply:GetVelocity().x/math.max(1,(GetConVar("sv_dynamicspeed_jump_distance"):GetFloat())),-ply:GetVelocity().y/math.max(1,GetConVar("sv_dynamicspeed_jump_distance"):GetFloat()),0))
 	end
 
@@ -116,9 +123,46 @@ hook.Add("SetupMove", "dynamicspeed_think", function(ply,mv)
 		ply:SetSlowWalkSpeed(maxspeed)
 	end
 
-	ply.ds_actualMaxSpeed = maxspeed
-	ply:SetDuckSpeed(0.3)
-	ply:SetUnDuckSpeed(0.3)
+	if ply:Crouching() then
+		maxspeed = maxspeed * ply:GetCrouchedWalkSpeed()
+	end
+
+	ply.ds_actualMaxSpeed = maxspeed 
+	ply:SetDuckSpeed(GetConVar("sv_dynamicspeed_crouch_speed"):GetFloat())
+	ply:SetUnDuckSpeed(GetConVar("sv_dynamicspeed_crouch_speed"):GetFloat())
+
+	print(ply.ds_actualMaxSpeed)
+
+end)
+
+
+hook.Add("SetupMove", "footsteps_play_always", function(ply,mv,cmd)
+
+	if GetConVar("sv_dynamicspeed_footstep_workaround"):GetInt() == 0 then return end
+
+	local shouldPlay = false
+	local moving = (ply:KeyDown(IN_FORWARD) or ply:KeyDown(IN_BACK) or ply:KeyDown(IN_MOVELEFT) or ply:KeyDown(IN_MOVERIGHT))
+	ply.ds_StepTimer = math.max(ply.ds_StepTimer - 1000 * FrameTime(), 0)
+
+	-- https://github.com/lua9520/source-engine-2018-hl2_src/blob/3bf9df6b2785fa6d951086978a3e66f49427166a/game/shared/baseplayer_shared.cpp#L770
+	if ply:Crouching() then
+		shouldPlay = (ply.ds_actualMaxSpeed < 60)
+	else
+		shouldPlay = (ply.ds_actualMaxSpeed < 90)
+	end
+
+	if moving and ply:OnGround() and shouldPlay and ply.ds_StepTimer == 0 then
+		local fMaxSpeed = ply.ds_actualMaxSpeed
+		local fStepTime = (1/fMaxSpeed)*GetConVar("sv_dynamicspeed_stepsoundtime_multiplier"):GetInt() + GetConVar("sv_dynamicspeed_stepsoundtime_offset"):GetInt() - 200
+
+		if ply:Crouching() then
+			fStepTime = fStepTime * ply:GetCrouchedWalkSpeed() + 200
+		end
+
+		ply:PlayStepSound(0.5)
+
+		ply.ds_StepTimer = fStepTime
+	end
 end)
 
 hook.Add("PlayerStepSoundTime", "dynamicspeed_stepsoundtime", function(ply, iType, bWalking)
@@ -136,6 +180,8 @@ hook.Add("PlayerStepSoundTime", "dynamicspeed_stepsoundtime", function(ply, iTyp
 	if (ply:Crouching()) then
 		fStepTime = fStepTime + 50
 	end
+
+	ply.ds_StepTimer = fStepTime
 
 	return fStepTime
 end)
